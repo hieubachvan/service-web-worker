c2384e4c9fa0549cf337d75c3745b6f877a4b1f8
hieubv <hieubv@vng.com.vn>
2023-04-11 14:06:30 +0700

    feat

--- src/components/movies/index.js
+++ src/components/movies/index.js
@@ -1,36 +1,42 @@
-import React, { useEffect, useState } from 'react';
-import axios from 'axios';
-import Movie from './movie';
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import Movie from "./movie";
 
 const Movies = () => {
-
   const [movies, setMovies] = useState([]);
   const [loading, setLoading] = useState(false);
 
   useEffect(() => {
     setLoading(true);
 
-    axios.get(`https://api.themoviedb.org/3/discover/tv`, {
-      params: {
-        sort_by: 'popularity.desc',
-        api_key: process.env.API_KEY
-      }
-    })
-      .then( (response) => {
+    axios
+      .get(`https://api.themoviedb.org/3/discover/tv`, {
+        params: {
+          sort_by: "popularity.desc",
+          api_key: "002c2746df4e80f1ff495f49bce6f7d9",
+        },
+      })
+      .then((response) => {
         setLoading(false);
         setMovies(response?.data?.results);
       })
-      .catch( (error) => {
+      .catch((error) => {
         setLoading(false);
         console.log(error);
-      })
-  }, [])
+      });
+  }, []);
 
   return (
-      <div className="flex flex-wrap -mb-4">
-        {!loading && movies?.length ? movies.map( (movie, index) =>  <Movie key={movie?.id ?? index} movie={movie}/>) : <h2>Loading...</h2>}
-      </div>
-  )
-}
+    <div className="flex flex-wrap -mb-4">
+      {!loading && movies?.length ? (
+        movies.map((movie, index) => (
+          <Movie key={movie?.id ?? index} movie={movie} />
+        ))
+      ) : (
+        <h2>Loading...</h2>
+      )}
+    </div>
+  );
+};
 
-export default Movies
+export default Movies;
