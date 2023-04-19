fdbbbd0b1209f3d19c2276d8822aa9d7212c245d
hieubv <hieubv@vng.com.vn>
2023-04-19 14:14:46 +0700

    update test

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 import App from "./App";
-import registerServiceWorker from './serviceWorkerRegistration';
+import registerServiceWorker from "./serviceWorkerRegistration";
 
-ReactDOM.render( <App/>, document.getElementById('root') );
+ReactDOM.render(<App />, document.getElementById("root"));
 
 registerServiceWorker();
